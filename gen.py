150902ed31a2b2845d60669c36c842f9221fcb62
mkreem <haruma221@icloud.com>
2024-09-26 20:07:18 +0900

    ok

--- gen.py
+++ gen.py
@@ -13,14 +13,14 @@ for filepath in header_dir.rglob('*.hpp'):
         continue
     if 'ac-library' in filepath.parts:
         continue
-    include_path = f'/home/mkreem/.pml/github.com/mkreem_library/{filepath.relative_to(header_dir).as_posix()}'
+    include_path = f'/home/pomelo/github.com/mkreem_library/{filepath.relative_to(header_dir).as_posix()}'
     header_files.append(include_path)
 
 with master_header.open('w') as f:
     f.write('#ifndef all_includes_HPP\n')
     f.write('#define all_includes_HPP\n\n')
-    
+
     for header in sorted(header_files):
         f.write(f'#include "{header}"\n')
-    
+
     f.write('\n#endif // all_includes_HPP\n')
