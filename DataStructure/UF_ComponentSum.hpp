eb6f65ebb09c12530995cf032d3bab50e45dd888
mkreem <haruma221@icloud.com>
2024-11-27 20:14:07 +0900

    修正

--- DataStructure/UF_ComponentSum.hpp
+++ DataStructure/UF_ComponentSum.hpp
@@ -13,7 +13,7 @@ using merge_function = void(*)(T& component_sum1, T component_sum2);
 
 // 型引数、非型引数（コンパイル時に定まるものに限る）
 template <typename T, merge_function<T> f>
-class UF_ComponentSum : UnionFind { // 継承
+class UF_ComponentSum : public UnionFind { // 継承
     std::vector<T> sum_;
 
 public:
@@ -24,9 +24,9 @@ public:
     /**
      * @brief u が所属する連結成分の sum と v が所属する連結成分の sum を統合する
      */
-    bool merge (const int& u, const int& v) {
+    bool merge(const int& u, const int& v) {
         int prev_root_u = root(u), prev_root_v = root(v);
-        bool merged = merge(u, v);
+        bool merged = UnionFind::merge(u, v);
         if (merged) {
             int root_u = root(u);
             /*
@@ -35,18 +35,18 @@ public:
             ・prev_root_u != root_u -> 連結後の成分の根は prev_root_v
             */
             if (prev_root_u != root_u) {
-                f(sum_[prev_root_v], std::move[prev_root_u]);
+                f(sum_[prev_root_v], std::move(sum_[prev_root_u]));
             } else {
-                f(sum_[prev_root_u], std::move[prev_root_v]);
+                f(sum_[prev_root_u], std::move(sum_[prev_root_v]));
             }
         }
 
         return merged;
     }
 
-    T sum (const int& v) const {
+    T sum(const int& v) {
         int root_v = root(v);
-        return sum_(v);
+        return sum_[root_v];
     }
 
 };
