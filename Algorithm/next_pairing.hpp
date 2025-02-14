a4c8d3179185e2b834e2272be212d4a01ed001bd
mkr33m <haruma221@icloud.com>
2025-02-14 18:32:35 +0900

    修正

--- Algorithm/next_pairing.hpp
+++ Algorithm/next_pairing.hpp
@@ -12,18 +12,18 @@ verify
 #include "../Math/math.hpp"
 
 template <typename T>
-bool next_pairing(const std::vector<T>& vec){
+bool next_pairing(const std::vector<T>& vec) {
     int N = vec.size();
     /**
      * @param used vec を右から左へ走査したとき、どの数字を見たか
      */
     ll used = 0;
-    for(int i = N - 1; i >= 0; i--){
+    for (int i = N - 1; i >= 0; i--) {
         used |= (1 << vec[i]);
-        if(i % 2 == 1 && vec[i] < math::log2_floor(used)){
+        if (i % 2 == 1 && vec[i] < math::log2_floor(used)) {
             vec[i] = __builtin_ctzll(used >> (vec[i] + 1)) + vec[i] + 1; // まだ見ていない数字のうち、 vec[i] より大きいものの中で最小のもの
             used ^= (1 << vec[i]);
-            for(int j = i + 1; j < N; j++){ // i より後ろを辞書順最小に
+            for (int j = i + 1; j < N; j++) { // i より後ろを辞書順最小に
                 vec[j] = __builtin_ctzll(used);
                 used ^= (1 << vec[j]);
             }
